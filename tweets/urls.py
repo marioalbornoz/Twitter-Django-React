5a3db3ffca416943783a79dadfebb964d63deec2
marioalbornoz <mario.albornozc@utem.cl>
2020-05-05 23:30:22 -0400

    conectando backend y frontend mediante vista basa en funcion y javascript

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from .views import home_view, tweet_detail_view
+from .views import home_view, tweet_detail_view, tweet_list_view
 
 app_name = 'tweets_app'
 
 urlpatterns = [
     path('', home_view),
+    path('tweets/', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -5,14 +5,26 @@ from .models import Tweet
 
 # Create your views here.
 
+
 def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1> Hola Mundo </h1>")
     return render(request,  'pages/home.html', context={}, status=200)
 
-def tweet_detail_view(request,tweet_id, *args, **kwargs):
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        'isUser': False,
+        'response': tweets_list
+    }
+    return JsonResponse(data)
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
 
     data = {
-        'tweet_id':tweet_id,
+        'tweet_id': tweet_id,
     }
     status = 200
     try:
@@ -21,5 +33,5 @@ def tweet_detail_view(request,tweet_id, *args, **kwargs):
     except:
         data['message'] = 'Not found'
         status = 404
-    
-    return JsonResponse(data, status = status)
+
+    return JsonResponse(data, status=status)
