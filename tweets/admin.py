881bbee2a184fbbf589df2e1fec15f0667d77932
marioalbornoz <mario.albornozc@utem.cl>
2020-05-13 20:18:20 -0400

    admin config

--- tweets/admin.py
+++ tweets/admin.py
@@ -4,4 +4,10 @@ from .models import Tweet
 
 # Register your models here.
 
-admin.site.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'user']
+    search_fields = ['user__username','user__email']
+    class Meta:
+        model = Tweet
+
+admin.site.register(Tweet, TweetAdmin)
--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
 
 # Create your models here.
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=255, null=True, blank=True)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-id']
+    def __str__(self):
+        return self.content
+    
